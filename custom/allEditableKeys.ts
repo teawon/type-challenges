ba592791a01f6937bb699ab3896c05ba572b4c4d
teawon <xodnjsvkvk9@gmail.com>
2024-09-01 14:52:17 +0900

    fix: allEditableKeys 수정

--- custom/allEditableKeys.ts
+++ custom/allEditableKeys.ts
@@ -5,27 +5,41 @@
  */
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-type AllEditableKeys<T> = {
-    [P in keyof T as Equal<ReadOnly<T[P]>, T[P] extends true ? P : never] : any
-}
+type AllEditableKeys<T> = keyof {
+  [P in keyof T as Equal<Readonly<Pick<T, P>>, Pick<T, P>> extends true
+    ? never
+    : P]: any;
+};
 
 type cases = [
-  Expect<Equal<AllEditableKeys<{ test1: never }>, 'test1'>>,
-  Expect<Equal<AllEditableKeys<{ test1: number, readonly test2: string }>, 'test1'>>,
-  Expect<Equal<AllEditableKeys<{ test1: object, readonly test2?: undefined, test3: null, test4: string }>, 'test1' | 'test3' | 'test4'>>,
-  Expect<Equal<AllEditableKeys<{}>, never>>,
-]
+  Expect<Equal<AllEditableKeys<{ test1: never }>, "test1">>,
+  Expect<
+    Equal<AllEditableKeys<{ test1: number; readonly test2: string }>, "test1">
+  >,
+  Expect<
+    Equal<
+      AllEditableKeys<{
+        test1: object;
+        readonly test2?: undefined;
+        test3: null;
+        test4: string;
+      }>,
+      "test1" | "test3" | "test4"
+    >
+  >,
+  Expect<Equal<AllEditableKeys<{}>, never>>
+];
 
 /**
- *  사용 사례 - 변경 가능한 키를 확인 
+ *  사용 사례 - 변경 가능한 키를 확인
  */
 
 interface User {
-    id: string;             // 불변 (immutable)
-    name: string;           // 변경 가능 (mutable)
-    readonly createdAt: Date; // 불변
-  }
-  
-  type UserMutableKeys = AllEditableKeys<User>; // 'name'
+  id: string; // 불변 (immutable)
+  name: string; // 변경 가능 (mutable)
+  readonly createdAt: Date; // 불변
+}
+
+type UserMutableKeys = AllEditableKeys<User>; // 'name'
